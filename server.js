e802da2acd862447b23e0d92a066dda2ebbf42af
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-03-22 17:02:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", (req,res) => {
   res.send(
-    "<h2>COVID19-Graphql</h2><p>By <a href='https://github.com/sharadcodes'>sharadcodes</a></p><p>By <a href='https://sharad-gql-covid19.herokuapp.com/graphql'>https://sharad-gql-covid19.herokuapp.com/graphql</a></p>")
+    "<h2>COVID19-Graphql</h2><p>By <a href='https://github.com/sharadcodes'>sharadcodes</a></p><p>GOTO: <a href='https://sharad-gql-covid19.herokuapp.com/graphql'>https://sharad-gql-covid19.herokuapp.com/graphql</a></p>")
 });
 
 app.use(
