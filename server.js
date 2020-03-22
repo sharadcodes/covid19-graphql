5a4ff9846164d2a6f9590ade716d67678ca9641a
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-03-22 17:02:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const expressGraphQL = require("express-graphql");
 const schema = require("./schema.js");
 const app = express();
 
+app.get("/", (req,res) => {
+  res.send(
+    "<h2>COVID19-Graphql</h2><p>By <a href='https://github.com/sharadcodes'>sharadcodes</a></p><p>By <a href='https://sharad-gql-covid19.herokuapp.com/graphql'>https://sharad-gql-covid19.herokuapp.com/graphql</a></p>")
+});
+
 app.use(
   "/graphql",
   expressGraphQL({
